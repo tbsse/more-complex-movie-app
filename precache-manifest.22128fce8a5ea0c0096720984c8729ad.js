141689298379955ea96b484eff806539badcda89
Richard <sjjb53@gmail.com>
2020-06-21 16:58:05 -0700

    Updates

---
+++ precache-manifest.22128fce8a5ea0c0096720984c8729ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908b9a313c12e334935bcc1e1f92a44c",
+    "url": "/more-complex-movie-app/index.html"
+  },
+  {
+    "revision": "bab0a787d70807f448b0",
+    "url": "/more-complex-movie-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d59893dbabce08d1ef3d",
+    "url": "/more-complex-movie-app/static/js/2.5a1b3253.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/more-complex-movie-app/static/js/2.5a1b3253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab0a787d70807f448b0",
+    "url": "/more-complex-movie-app/static/js/main.9f99faa3.chunk.js"
+  },
+  {
+    "revision": "2e0be2ed393fc44ddc76",
+    "url": "/more-complex-movie-app/static/js/runtime-main.74effa3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/more-complex-movie-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
